6ed72f4f98f124ea4b8b85bac9d146d419c35c71
boxp <tiyotiyouda@gmail.com>
2018-03-09 01:10:04 +0900

    Add dummy response

--- arks-gallery-server/src/arks_gallery_server/app/resolvers.cljs
+++ arks-gallery-server/src/arks_gallery_server/app/resolvers.cljs
@@ -1,32 +1,78 @@
 (ns arks-gallery-server.app.resolvers)
 
-(def authors [{:id 1 :firstName "Tom" :lastName "Coleman"}
-              {:id 2 :firstName "Sashiko" :lastName "Stubailo"}])
+(def users (clj->js [{:id 1 :name "marisa" :ship_num 3 :profile_img_url "dummy" :following [2] :followers [2]}
+                     {:id 2 :name "alice" :ship_num 3 :profile_img_url "dummy" :following [1] :followers [1]}
+                     {:id 3 :name "reimu" :ship_num 3 :profile_img_url "dummy" :following [1] :followers [1]}
+                     {:id 4 :name "ru-mia" :ship_num 3 :profile_img_url "dummy" :following [1] :followers [1]}]))
 
-(def posts [{:id 1 :authorId 1 :title "Introdution to GraphQL" :votes 2}
-            {:id 2 :authorId 2 :title "GraphQL Rocks" :votes 3}
-            {:id 3 :authorId 2 :title "Advanced GraphQL" :votes 1}])
+(def ships (clj->js [{:num 3 :name "ソーン"}]))
+
+(def images (clj->js [{:id 1 :name "hoge" :url "dummy" :tweet_count 0 :comment_ids [1] :tag_ids [1] :like_ids [1]}]))
+
+(def comments (clj->js [{:id 1 :text "hogehoge" :reply_to_id nil}]))
+
+(def tags (clj->js [{:id 1 :name "hoge" :owner_id 1}]))
+
+(def comment_likes (clj->js [{:from_id 1 :to_id 1}]))
+(def image_likes (clj->js [{:from_id 1 :to_id 1}]))
+
+(def follows (clj->js [{:from_id 1 :to_id 2}
+                       {:from_id 2 :to_id 1}]))
 
 (def resolve-functions
   (clj->js
     {:Query
-     {:posts (fn [_ _] posts)
-      :author (fn [_ r]
-                (->> authors
-                     (filter #(= (.-id %) (.-id r)))
-                     first))}
-     :Mutation
-     {:upvotePost (fn [_ r]
-                    (->> posts
-                         (filter #(= (.-id %) (.-postId r)))
+     {:images (fn []
+                (clj->js images))
+      :users (fn []
+               (clj->js users))}
+     :User
+     {:ship (fn [user]
+              (->> ships
+                   (filter #(= (.-num %) (.-ship_num user)))
+                   first))
+      :following (fn [user]
+                   (->> follows
+                        (filter #(and ((-> user .-following set) (.-to_id %))))))
+      :followers (fn [user]
+                   (->> follows
+                        (filter #((-> user .-following set) (.-to_id %)))))
+      }
+     :Image
+     {:comments (fn [image]
+                  (->> comments
+                       (filter #((-> image .-comment_ids set) (.-id %)))))
+      :tags (fn [image]
+              (->> tags
+                   (filter #((-> image .-tag_ids set) (.-id %)))))
+      :likes (fn [image]
+               (->> image_likes
+                    (filter #((-> image .-like_ids set) (.-to_id %)))))}
+     :Comment {:reply_to (fn [comment]
+                           (->> comments
+                                (filter #(= (-> comment .-reply_to_id) (.-id %)))
+                                first))
+               :likes (fn [comment]
+                        (->> comment_likes
+                             (filter #((-> comment .-likes set) (.-to_id %)))))}
+     :Tag {:owner (fn [tag]
+                    (->> users
+                         (filter #(= (.-owner_id tag) (.-id %)))
                          first))}
-     :Author
-     {:posts (fn [author]
-               (->> posts
-                    (filter #(= (.-id %) (.-id author)))
-                    first))}
-     :Post
-     {:author (fn [post]
-                (->> authors
-                     (filter #(= (.-id %) (.-authorId post)))
-                     first))}}))
+     :Like {:from (fn [like]
+                    (->> users
+                         (filter #(= (.-from_id like) (.-id %)))
+                         first))
+            :to (fn [like]
+                  (->> users
+                       (filter #(= (.-to_id like) (.-id %)))
+                       first))}
+     :Follow {:from (fn [follow]
+                      (->> users
+                           (filter #(= (.-from_id follow) (.-id %)))
+                           first))
+              :to (fn [follow]
+                    (->> users
+                         (filter #(= (.-to_id follow) (.-id %)))
+                         first))
+     }}))
