490ec632db48221c2ecd6ff3a0481f6148a0465e
boxp <tiyotiyouda@gmail.com>
2018-03-09 01:09:10 +0900

    Add support figwheel

--- arks-gallery-server/dev/cljs/user.cljs
+++ arks-gallery-server/dev/cljs/user.cljs
@@ -1,14 +1,38 @@
 (ns cljs.user
-  (:require [arks-gallery-server.app.server :refer [server]]))
+  (:require [arks-gallery-server.app.server :refer [server]]
+            [cljs.nodejs :as nodejs]))
+
+(nodejs/enable-util-print!)
+
+(defonce p (atom 9000))
+(defonce s (atom nil))
 
 (defn -serve
+  [port]
+  (let [server (if port
+                 (.listen (server) port)
+                 (.listen (server) @p))]
+    (->> server .address .-port (println "listening to PORT:"))
+    server))
+
+(defn start
+  [port]
+  (when-not @s
+    (when port (reset! p port))
+    (reset! s (-serve port))))
+
+(defn stop []
+  (when @s
+    (-> @s .close)
+    (reset! s nil)))
+
+(defn reload []
+  (stop)
+  (start @p))
+
+(defn -main
   [& args]
   (let [port (some-> args first js/parseInt)]
-    (if port
-      (->> (.listen (server) port)
-           .address
-           .-port
-           (println "listening to PORT:")
-      (println "port number has no exists.")))))
+    (start port)))
 
-(set! *main-cli-fn* -serve)
+(set! *main-cli-fn* -main)
